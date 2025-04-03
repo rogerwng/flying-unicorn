db346f776b51ccf6520f1be488c1ccb6a5857090
rogerwang <rogerwang9@ucla.edu>
2025-04-03 14:35:25 -0700

    add files

---
+++ sensor_verification/gy273_verify/Core/Src/gy273.c
@@ -0,0 +1,10 @@
+/**
+  ******************************************************************************
+  * @file           : gy273.c
+  * @brief          : GY273 Interface
+  *                   This file contains the functions used to configure and
+  *                   read data from GY273.
+  ******************************************************************************
+  */
+
+#include "gy273.h"
--- sensor_verification/gy273_verify/Core/Src/main.c
+++ sensor_verification/gy273_verify/Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "gy273.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/

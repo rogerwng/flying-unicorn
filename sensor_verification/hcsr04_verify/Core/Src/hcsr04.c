5045653cff803dceb7997174f6b50771ce80fe63
rogerwang <rogerwang9@ucla.edu>
2025-06-16 22:37:36 -0700

    ultra echoIT routine

--- sensor_verification/hcsr04_verify/Core/Src/hcsr04.c
+++ sensor_verification/hcsr04_verify/Core/Src/hcsr04.c
@@ -9,10 +9,17 @@
 
 #include "hcsr04.h"
 
+#define HCSR04_CAPTURE_TIMEOUT_US 600
+
 // capture flag set when interrupt detects falling edge (end of echo signal)
 static uint8_t captureFlag = 0;
+static uint32_t t1;
+static uint32_t t2;
 // measurement data
 static float distance;
+// compensation constants
+uint32_t timerPrescaler;
+uint32_t clkFreq;
 
 // peripheral handles
 static TIM_HandleTypeDef* myhtim;
@@ -22,6 +29,7 @@ static uint16_t triggerPin;
 
 /** static function declarations */
 static void delay_us(uint16_t t);
+static float calculateTime(uint32_t time1, uint32_t time2);
 
 /**	Initializing sensor by saving TIM and GPIO handles
  * 	INPUTS:
@@ -34,6 +42,14 @@ void hcsr04_init(TIM_HandleTypeDef* htim, uint32_t channel, GPIO_TypeDef* GPIOx,
 	timChannel = channel;
 	myGPIO = GPIOx;
 	triggerPin = tPin;
+
+	timerPrescaler = myhtim->Init.Prescaler;
+	clkFreq = HAL_RCC_GetPCLK1Freq();
+
+	// timer clock doubled when APB1 prescaler > 1
+	if ((RCC->CFGR & RCC_CFGR_PPRE1) != RCC_CFGR_PPRE1_DIV1) {
+		clfFreq *= 2;
+	}
 }
 
 /**	Delay function for microsecond delays using timer
@@ -52,6 +68,24 @@ static void delay_us(uint16_t t) {
 	HAL_TIM_Base_Stop(myhtim);
 }
 
+/** Function to calculate time from tick differences
+ * 	INPUTS:
+ * 		time1 - first tick
+ * 		time2 - second tick
+ * 	OUTPUTS:
+ * 		time in us
+ */
+static float calculateTime(uint32_t time1, uint32_t time2) {
+	uint32_t diff;
+	if (time2 >= time1) {
+		diff = time2 - time1;
+	} else {
+		diff = (__HAL_TIM_GET_AUTORELOAD(myhtim) - time2 + time1 + 1); // handle overflow
+	}
+
+	float time_us = (diff * (timerPrescaler + 1)) / clkFreq;
+}
+
 /**	Trigger sensor and enable interrupt
  * */
 void hcsr04_trigger() {
@@ -67,5 +101,46 @@ void hcsr04_trigger() {
 /**	IC Interrupt Routine
  * */
 void hcsr04_echo_IT() {
+	// 1st capture (rising edge) - start of echo
+	if (!captureFlag) {
+		t1 = HAL_TIM_ReadCapturedValue(myhtim, timChannel);
 
+		__HAL_TIM_SET_CAPTUREPOLARITY(myhtim, timChannel, TIM_INPUTCHANNELPOLARITY_FALLING);
+
+		captureFlag = 1;
+
+	} else if (captureFlag) {
+		// 2nd capture (falling edge) - echo complete
+		t2 = HAL_TIM_ReadCapturedValue(myhtim, timChannel);
+
+		HAL_TIM_IC_Stop_IT(myhtim, timChannel);
+
+		// convert tick counts to seconds
+		float time_us = calculateTime(t1, t2);
+
+		// update distance
+		__disable_irq();
+		distance = time_us/58.0f;
+		__enable_irq();
+
+		captureFlag = 0;
+		__HAL_TIM_SET_CAPTUREPOLARITY(myhtim, timChannel, TIM_INPUTCHANNELPOLARITY_RISING);
+	}
+}
+
+/** Checking for hanging echo, if hanging, stops IT
+ * 	OUTPUT:
+ * 		0 (not hanging), 1 (hanging)
+ */
+uint8_t hcsr04_hangCheck() {
+	if (captureFlag) {
+		uint32_t t = HAL_TIM_ReadCapturedValue(myhtim, timChannel);
+
+		float time_us = calculateTime(t1, t);
+		if (time_us >= HCSR04_CAPTURE_TIMEOUT_US) {
+			captureFlag = 0;
+			HAL_TIM_IC_Stop_IT(myhtim, timChannel);
+			__HAL_TIM_SET_CAPTURE_POLARITY(myhtim, timChannel, TIM_INPUTCHANNELPOLARITY_RISING);
+		}
+	}
 }

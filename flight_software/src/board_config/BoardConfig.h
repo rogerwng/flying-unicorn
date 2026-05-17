7399b2790c0dfabd264cf48d9cc835c47844604b
roger <rogerwng9@gmail.com>
2026-05-17 00:55:38 +0000

    add imu spi handle

--- flight_software/src/board_config/BoardConfig.h
+++ flight_software/src/board_config/BoardConfig.h
@@ -7,9 +7,11 @@
 // ===========================
 
 extern UART_HandleTypeDef huart1;
+extern SPI_HandleTypeDef hspi2;
 
 // ===========================
 // HANDLE ALIASES
 // ===========================
 
 inline constexpr UART_HandleTypeDef* LOGGER_HUART = &huart1;
+inline constexpr SPI_HandleTypeDef* IMU_SPI = &hspi2;
